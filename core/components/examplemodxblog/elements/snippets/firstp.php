1444e8f32a69cc4d5563bf4fea23b8c2b20d3f3e
Gravino Design <gravino81@users.noreply.github.com>
2020-08-10 11:48:53 +0200

    Hello, this change solve a utf8 problems

--- core/components/examplemodxblog/elements/snippets/firstp.php
+++ core/components/examplemodxblog/elements/snippets/firstp.php
@@ -1,7 +1,7 @@
 <?php
 $dom = new DOMDocument;
 libxml_use_internal_errors(true);
-$dom->loadHtml($input);
+$dom->loadHtml('<?xml encoding="utf-8" ?>' .$input);
 $ps = $dom->getElementsByTagName('p');
 foreach ($ps as $p) {
     return $p->nodeValue;
